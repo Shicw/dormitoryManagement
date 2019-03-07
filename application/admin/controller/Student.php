625c822d4b837c2c7d045ca4e87f76c156222f93
shicw <362559994@qq.com>
2019-03-07 15:40:45 +0800

    完成退宿办理

--- application/admin/controller/Student.php
+++ application/admin/controller/Student.php
@@ -9,6 +9,7 @@ namespace app\admin\controller;
 use app\admin\model\StudentModel;
 use app\AdminBaseController;
 use think\Controller;
+use think\Db;
 
 class Student extends AdminBaseController
 {
@@ -40,10 +41,49 @@ class Student extends AdminBaseController
      * 登记退宿页面
      */
     public function checkOut(){
+        $buildingId = getAdmin()['building_id'];
+        $this->assign('buildingId',$buildingId);
         return $this->fetch();
     }
     /**
      * 登记退宿提交
      */
-    public function checkOutPost(){}
+    public function checkOutPost(){
+        if(!$this->request->isPost()){
+            $this->error('请求失败');
+        }
+        $studentId = $this->request->post('studentId');
+        $model = new StudentModel();
+        $result = $model->doCheckOut($studentId);
+        if($result['code']===1){
+            $this->success($result['msg']);
+        }else{
+            $this->error($result['msg']);
+        }
+    }
+
+    /**
+     * 根据学号获取学生住宿信息
+     */
+    public function getStudentDetail(){
+        if(!$this->request->isGet()){
+            $this->error('请求失败');
+        }
+        $studentId = $this->request->get('studentId');
+
+        $found = Db::name('student s')
+            ->field(['s.*','d.dormitory_number','db.bed_number'])
+            ->join([
+                ['dormitory d','d.id=s.dormitory_id'],
+                ['dormitory_bed db','db.id=s.bed_id'],
+            ])
+            ->where(['student_id'=>$studentId,'s.delete_time'=>0])
+            ->find();
+        if($found){
+            $found['birthday'] = date("Y-m-d",$found['birthday']);
+            $this->success('请求成功','',$found);
+        }else{
+            $this->error('无该学生住宿信息');
+        }
+    }
 }
--- application/admin/model/StudentModel.php
+++ application/admin/model/StudentModel.php
@@ -13,11 +13,17 @@ use think\Model;
 class StudentModel extends Model
 {
     /**
-     * 等级入住提交
+     * 办理入住
      * @param $data
      * @return array
      */
     public function doCheckIn($data){
+        //判断宿舍是否满员
+        $found = $this->name('dormitory')->where(['id'=>$data['dormitory_id']])->find();
+        if($found['real_student_count']===$found['max_student_count']){
+            return ['code' => 0, 'msg' => '宿舍已满员'];
+        }
+
         $this->startTrans();//开启事务处理
         try{
             //写入信息
@@ -39,13 +45,55 @@ class StudentModel extends Model
             //该床位status设为1
             $result = $this->name('dormitory_bed')->where(['id'=>$data['bed_id']])->update(['status'=>1]);
             if (!$result) {
-                throw new Exception("登记失败");
+                throw new Exception("办理入住失败");
+            }
+            //宿舍实际人数+1
+            $result = $this->name('dormitory')->where(['id'=>$data['dormitory_id']])->setInc('real_student_count',1);
+            if (!$result) {
+                throw new Exception("办理入住失败");
             }
             $this->commit();// 提交事务
         }catch(Exception $e){
             $this->rollback();// 回滚事务
             return ['code' => 0, 'msg' => $e->getMessage()];
         }
-        return ['code' => 1, 'msg' => '登记成功'];
+        return ['code' => 1, 'msg' => '办理入住成功'];
+    }
+
+    /**
+     * 办理退宿
+     * @param $studentId
+     * @return array
+     */
+    public function doCheckOut($studentId){
+        //判断该学生信息是否存在
+        $found = $this->name('student')->where(['student_id'=>$studentId,'delete_time'=>0])->find();
+        if(!$found){
+            return ['code' => 0, 'msg' => '该学生住宿信息不存在'];
+        }
+
+        $this->startTrans();//开启事务处理
+        try{
+            //删除学生住宿信息
+            $result = $this->name('student')->where('student_id',$studentId)->update(['delete_time'=>time()]);
+            if(!$result){
+                throw new Exception("办理退宿失败");
+            }
+            //该床位status设为0
+            $result = $this->name('dormitory_bed')->where(['id'=>$found['bed_id']])->update(['status'=>0]);
+            if (!$result) {
+                throw new Exception("办理退宿失败");
+            }
+            //宿舍实际人数-1
+            $result = $this->name('dormitory')->where(['id'=>$found['dormitory_id']])->setDec('real_student_count',1);
+            if (!$result) {
+                throw new Exception("办理退宿失败");
+            }
+            $this->commit();// 提交事务
+        }catch(Exception $e){
+            $this->rollback();// 回滚事务
+            return ['code' => 0, 'msg' => $e->getMessage()];
+        }
+        return ['code' => 1, 'msg' => '办理退宿成功'];
     }
 }
